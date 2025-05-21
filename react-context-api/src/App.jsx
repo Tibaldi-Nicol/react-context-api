bad0ba6adebadceda2ad6cfa48a1592795cb5ff3
Tibaldi-Nicol <nickkoltibaldi@gmail.com>
2025-05-21 16:25:59 +0200

    [UPD]Add-new-app.jsx

---
+++ react-context-api/src/App.jsx
@@ -0,0 +1,42 @@
+// App.jsx
+import { createContext, useState } from "react";
+import PostsPage from "./pages/PostsPage";
+
+ const PostsContext = createContext();
+
+function App() {
+  const [posts] = useState([
+    {
+      id: 1,
+      title: "React Basis",
+      content: "React apps are made out of components...",
+      category: "FrontEnd",
+    },
+    {
+      id: 2,
+      title: "What is Express.js?",
+      content: "Express is a minimal and flexible Node.js...",
+      category: "Backend",
+    },
+    {
+      id: 3,
+      title: "Run JavaScript Everywhere",
+      content: "Node.js® is a free, open-source...",
+      category: "Backend",
+    },
+    {
+      id: 4,
+      title: "Build fast, responsive sites with Bootstrap",
+      content: "Powerful, extensible frontend toolkit...",
+      category: "Frontend",
+    },
+  ]);
+
+  return (
+    <PostsContext.Provider value={posts}>
+      <PostsPage />
+    </PostsContext.Provider>
+  );
+}
+
+export default App;
