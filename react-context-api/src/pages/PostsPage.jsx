90a1ce0f901ee754a6b098bf4ef4dc29d47517bd
Tibaldi-Nicol <nickkoltibaldi@gmail.com>
2025-05-21 16:29:23 +0200

    [UPD]Add-post-pages

---
+++ react-context-api/src/pages/PostsPage.jsx
@@ -0,0 +1,13 @@
+// pages/PostsPage.jsx
+import PostsList from "../components/PostsList";
+
+function PostsPage() {
+  return (
+    <div>
+      <h1>Blog Posts</h1>
+      <PostsList />
+    </div>
+  );
+}
+
+export default PostsPage;
